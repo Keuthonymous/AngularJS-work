e05e5da08a0096e6fb63faad04eab9c7cf88987f
Keuthonymous <michaeldaughtrey@host-78-76-210-135.homerun.telia.com>
2017-06-15 14:10:42 +0200

    Added Angular module testApp

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+var app = angular.module("testApp" []); 
+
+var MainCtrl = function() {
+	
+	var product = {
+		Name: "Something",
+		Price: 22.22,
+		Description: "It's something"
+	};
+	
+};
+
+app.controller("MainCtrl", MainCtrl);
